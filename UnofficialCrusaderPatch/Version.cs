bfba66e2ac38c357faa7224d18b522931950e477
J-T-de <50496721+J-T-de@users.noreply.github.com>
2020-01-26 20:51:05 +0100

    Adding missing headers to Version.cs

--- UnofficialCrusaderPatch/Version.cs
+++ UnofficialCrusaderPatch/Version.cs
@@ -2138,6 +2138,44 @@ namespace UCP
                         new BinBytes(0x83, 0xE0, 0xE7, 0x83, 0xC0, 0x64, 0x90, 0x90)
                     }
                 },
+
+                new DefaultHeader("o_shfy_religion", false)
+                // fixes religion popularity bonus
+                {
+                    new BinaryEdit("o_shfy_religionpopularity")
+                    {
+                        new BinSkip(9),
+                        new BinBytes(0x83, 0xC1, 0x00),
+                        new BinSkip(9),
+                        new BinBytes(0x83, 0xC1, 0x00),
+                    },
+
+                    new BinaryEdit("o_shfy_religionpopularitytab")
+                    {
+                        new BinSkip(9),
+                        new BinBytes(0x83, 0xC6, 0x00),
+                        new BinSkip(9),
+                        new BinBytes(0x83, 0xC6, 0x00),
+                    },
+
+                    new BinaryEdit("o_shfy_religiontab")
+                    {
+                        new BinBytes(0xEB, 0x6D),
+                        new BinSkip(132),
+                        new BinBytes(0xB9, 0x00, 0x00, 0x00, 0x00),
+                        new BinSkip(9),
+                        new BinBytes(0x83, 0xC1, 0x00)
+                    }
+                },
+
+                new DefaultHeader("o_shfy_resourcequantity", false)
+                // fixes resource quantity
+                {
+                    new BinaryEdit("o_shfy_resourcequantity")
+                    {
+                        new BinBytes(0x83, 0xC0, 0x00)
+                    },
+                }
             },
 
             #endregion
