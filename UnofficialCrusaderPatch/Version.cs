1fb4358e923bda557bc5bcac82797dfda256f3fb
peterkohlberger <ckpengine@gmail.com>
2020-01-30 19:22:07 +0100

    Fix fire ballista bug.

--- UnofficialCrusaderPatch/Version.cs
+++ UnofficialCrusaderPatch/Version.cs
@@ -52,19 +52,28 @@ namespace UCP
             #region BUG FIXES
 
             /*
-             * BALLISTAS ATTACK MONKS AND TUNNELERS
+             * FIRE BALLISTAS ATTACK MONKS AND TUNNELERS
              */
 
-            new Change("u_ballistafix", ChangeType.Bugfix, true)
+            new Change("u_fireballistafix", ChangeType.Bugfix, true)
             {
-                new DefaultHeader("u_ballistafix")
+                new DefaultHeader("u_fireballistafix")
                 {
-                    new BinaryEdit("u_ballistatunneler")
+                    new BinaryEdit("u_fireballistatunneler")
                     {
-                        new BinSkip(3),
-                        new BinBytes(0xEB)
+                        new BinSkip(13),
+                        new BinHook(6)
+                        {
+                            0x83, 0xF8, 0x05, //  cmp eax,05
+                            0x66, 0x9C, //  pushf
+                            0x83, 0xC0, 0xEA, //  add eax,-16
+                            0x66, 0x9D, //  popf
+                            0x75, 0x05, //  jne short 5
+                            0xB8, 0x05, 0x00, 0x00, 0x00, //  mov eax,05
+                            0x83, 0xF8, 0x37, //  cmp eax,37
+                        },
                     },
-                    new BinaryEdit("u_ballistamonk")
+                    new BinaryEdit("u_fireballistamonk")
                     {
                         new BinBytes(0x00)
                     }
