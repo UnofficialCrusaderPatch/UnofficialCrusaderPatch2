e255fc0148ab20253acf402806e35827efbfe386
patel-nikhil <patenikh2@gmail.com>
2020-02-15 15:47:04 -0500

    Added command line arguments to build and updated logging of commands

---
+++ UnofficialCrusaderPatchConsole/PathUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCP
+{
+    class PathUtils
+    {
+        static Dictionary<String, String> installPaths;
+
+        static PathUtils()
+        {
+            String aicPath = "aic";
+            String aivPath = "aiv";
+            String mapsPath = "maps";
+            String mapsExtremePath = "mapsExtreme";
+
+            installPaths = new Dictionary<string, string>();
+            installPaths.Add("aic", aicPath);
+            installPaths.Add("aiv", aivPath);
+            installPaths.Add("maps", mapsPath);
+            installPaths.Add("mapsExtreme", mapsExtremePath);
+        }
+
+        public static String Get(String path)
+        {
+            String folder;
+            installPaths.TryGetValue(path, out folder);
+            return folder;
+        }
+    }
+}
--- UnofficialCrusaderPatchConsole/Program.cs
+++ UnofficialCrusaderPatchConsole/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 using UCP;
 using UCP.Patching;
 
@@ -8,23 +11,76 @@ namespace UCP
     {
         static void Main(string[] args)
         {
-                SilentInstall();
+            Configuration.LoadGeneral();
+            ResolvePath();
+            ResolveArgs(args);
+            SilentInstall();
+        }
+
+        static void ResolveArgs(String[] args)
+        {
+
+            Func<String, String, bool, bool, bool> fileTransfer = (src, dest, overwrite, log) =>
+            {
+                return FileUtils.Transfer(src, dest, overwrite, log);
+            };
+
+            bool silent = false;
+            foreach (String arg in args)
+            {
+                if (arg == "--no-output")
+                {
+                    silent = true;
+                }
+            }
+
+            foreach (String arg in args)
+            {
+                if (arg == "--no-output")
+                {
+                    continue;
+                } else if (!arg.StartsWith("--") || !arg.Contains("="))
+                {
+                    Console.WriteLine("Install failed. Invalid arguments provided.");
+                    return;
+                }
+                String srcPath = arg.Split('=')[1];
+                String rawOpt = arg.Split('=')[0].Substring(2);
+                bool overwrite = false;
+                if (rawOpt.Split('-').Length > 1 && rawOpt.Split('-')[1].ToLower().Contains("overwrite"))
+                {
+                    overwrite = true;
+                }
+                String opt = rawOpt.Split('-')[0];
+
+                fileTransfer(Path.Combine(Environment.CurrentDirectory, srcPath), Path.GetFullPath(Path.Combine(Configuration.Path, PathUtils.Get(opt))), overwrite, !silent);   
+            }
+        }
+
+        static void ResolvePath()
+        {
+            if (!Patcher.CrusaderExists(Configuration.Path))
+            {
+                if (Patcher.CrusaderExists(Environment.CurrentDirectory))
+                {
+                    Configuration.Path = Environment.CurrentDirectory;
+                }
+                else if (Patcher.CrusaderExists(Path.Combine(Environment.CurrentDirectory, "..\\")))
+                {
+                    Configuration.Path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "..\\"));
+                }
+            }
         }
 
         static void SilentInstall()
         {
-            Configuration.LoadGeneral();
             Configuration.LoadChanges();
-
-            if (!Patcher.CrusaderExists(Configuration.Path) && Patcher.CrusaderExists(Environment.CurrentDirectory))
-            {
-                Configuration.Path = Environment.CurrentDirectory;
-            }
             AICChange.LoadFiles();
             Configuration.LoadChanges();
             Version.Changes.Contains(null);
             Patcher.Install(Configuration.Path, null);
             Console.WriteLine("UCP successfully installed");
+            Console.WriteLine("Path to Stronghold Crusader is:" + Configuration.Path);
         }
     }
 }
