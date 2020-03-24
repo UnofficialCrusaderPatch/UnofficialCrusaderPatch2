3e2a49d7d23d7f182f39c208f2b24853b4ef393a
J-T-de <50496721+J-T-de@users.noreply.github.com>
2020-03-24 17:45:55 +0100

    fixed bug with default multiplay speed option

--- UnofficialCrusaderPatch/Version.cs
+++ UnofficialCrusaderPatch/Version.cs
@@ -2571,12 +2571,17 @@ namespace UCP
             
             new Change("o_default_multiplayer_speed", ChangeType.Other)
             {
-                // 878FB
                 new SliderHeader("o_default_multiplayer_speed", false, 20, 90, 1, 40, 40)
                 {
+                    // 878FB
                     new BinaryEdit("o_default_multiplayer_speed")
                     {
                         new BinSkip(16),
+                        new BinByteValue()
+                    },
+                    new BinaryEdit("o_default_multiplayer_speed_reset")
+                    {
+                        new BinSkip(6),
                         new BinByteValue()
                     }
                 }
