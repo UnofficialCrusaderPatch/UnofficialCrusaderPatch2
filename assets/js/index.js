72d56e6c23bbe24e79c84c3da732bb3f8075a0c0
Nikhil <12703092+patel-nikhil@users.noreply.github.com>
2020-06-20 08:12:52 -0400

    Updated FAQ (#608)
    * Updated AIV editor example video in FAQ
    
    * Converted faq to load q&a from json file for easy editing

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,44 +14,6 @@
 //   }, 500);
 // });
 
-$(".list-group-item").on("click", function() {
-  $(".list-group-item").removeClass("active");
-  $(this).addClass("active");
-});
-
-$(document).on('click', '.feature-filter', function (e) {
-  e.stopPropagation();
-  console.log("test");
-  if ($("#feature-filter-bugfix")[0].checked != true){
-    $("#feature-bugfix-header").addClass("d-none");
-    $(".row.feature-bugfix").addClass("d-none");
-  } else {
-    $("#feature-bugfix-header").removeClass("d-none");
-    $(".row.feature-bugfix").removeClass("d-none");
-  }
-  if ($("#feature-filter-ai")[0].checked != true){
-    $("#feature-ai-header").addClass("d-none");
-    $(".row.feature-ai").addClass("d-none");
-  } else {
-    $("#feature-ai-header").removeClass("d-none");
-    $(".row.feature-ai").removeClass("d-none");
-  }
-  if ($("#feature-filter-unit")[0].checked != true){
-    $("#feature-unit-header").addClass("d-none");
-    $(".row.feature-unit").addClass("d-none");
-  } else {
-    $("#feature-unit-header").removeClass("d-none");
-    $(".row.feature-unit").removeClass("d-none");
-  }
-  if ($("#feature-filter-misc")[0].checked != true){
-    $("#feature-misc-header").addClass("d-none");
-    $(".row.feature-misc").addClass("d-none");
-  } else {
-    $("#feature-misc-header").removeClass("d-none");
-    $(".row.feature-misc").removeClass("d-none");
-  }
-});
-
 
 
 // var aicFieldList = $.getJSON("res/fields.json");
@@ -82,60 +44,3 @@ function download(data, filename, type) {
       }, 0); 
   }
 }
-
-
-$(function(){
-    $("#aic-table").load("aic-description.html"); 
-});
-
-$(function(){
-  $("#aic-popularity").load("res/popularity.html"); 
-});
-
-$(function(){
-  $("#aic-taxes").load("res/taxes.html"); 
-});
-
-$(function(){
-  $("#aic-food").load("res/food.html"); 
-});
-
-$(function(){
-  $("#aic-farms").load("res/farms.html"); 
-});
-
-$(function(){
-  $("#aic-buildings").load("res/buildings.html"); 
-});
-
-$(function(){
-  $("#aic-build-efficiency").load("res/build-efficiency.html"); 
-});
-
-$(function(){
-  $("#aic-resource-management").load("res/resource-management.html"); 
-});
-
-$(function(){
-  $("#aic-weapons").load("res/weapons.html"); 
-});
-
-$(function(){
-  $("#aic-recruiting").load("res/recruiting.html"); 
-});
-
-$(function(){
-  $("#aic-attacking").load("res/attacking.html"); 
-});
-
-$(function(){
-  $("#aic-attack-siege").load("res/attack-siege.html"); 
-});
-
-$(function(){
-  $("#aic-defense").load("res/defense.html"); 
-});
-
-$(function(){
-  $("#aic-uncategorized").load("res/uncategorized.html"); 
-});
