27dcdf97640f0b1434bb11fc53175253f1bbf5db
LordHansCapon <59705872+LordHansCapon@users.noreply.github.com>
2020-10-12 23:33:58 +0200

    Added bugfix for fletchers where they would go back to workplace instead of the stockpile after they deposit the weapon. (#657)
    Co-authored-by: peterkohlberger <ckpengine@gmail.com>
    Co-authored-by: Krarilotus <51748815+Krarilotus@users.noreply.github.com>
    
    Works as intended, tested and signed!

--- UnofficialCrusaderPatch/Version.cs
+++ UnofficialCrusaderPatch/Version.cs
@@ -292,6 +292,23 @@ namespace UCP
                 }
             },
             
+          
+            /*
+             * Fletcher bugfix 
+             */
+            new Change("o_fix_fletcher_bug", ChangeType.Bugfix)
+            {
+                new DefaultHeader("o_fix_fletcher_bug")
+                {
+                    new BinaryEdit("o_fix_fletcher_bug")
+                    {
+                        new BinSkip(0x1E), // skip 30 bytes
+                        new BinBytes(0x01) // set state to 1 instead of 3
+
+                    }
+                }
+            },
+          
             // Fix AI crusader archers not lighting pitch
             new Change("ai_fix_crusader_archers_pitch", ChangeType.Bugfix, true)
             {
