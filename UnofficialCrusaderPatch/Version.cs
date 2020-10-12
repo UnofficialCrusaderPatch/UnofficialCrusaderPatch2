4c26e140fc1d871286e5571475c819b9f5bf43aa
LordHansCapon <59705872+LordHansCapon@users.noreply.github.com>
2020-10-12 18:10:54 +0200

    - Moat digging units are less likely to disappear while digging moat. (#637)
    Co-authored-by: peterkohlberger <ckpengine@gmail.com>
    
    this works as intended, well done!
    - tested on own moat with differnet unit types
    - tested on enemy moat with different unit types

--- UnofficialCrusaderPatch/Version.cs
+++ UnofficialCrusaderPatch/Version.cs
@@ -291,6 +291,26 @@ namespace UCP
 
                 }
             },
+            
+            // Fix moat digging unit disappearing
+            new Change("o_fix_moat_digging_unit_disappearing", ChangeType.Bugfix, true)
+            {
+                new DefaultHeader("o_fix_moat_digging_unit_disappearing")
+                {
+
+                    new BinaryEdit("o_fix_moat_digging_unit_disappearing")
+                    {
+                        new BinAddress("skip", 11),
+                        new BinHook(9)
+                        {
+                            0x83, 0xBC, 0x30, 0xD4, 0x08, 0x00, 0x00, 0x7D, // cmp dword ptr [eax+esi+8D4],7D
+                            0x74, 0x09, // je short 9
+                            0x66, 0x83, 0xBC, 0x30, 0xA8, 0x06, 0x00, 0x00, 0x01
+                        }
+                    }
+
+                }
+            },
             #endregion
 
             #region AI LORDS
