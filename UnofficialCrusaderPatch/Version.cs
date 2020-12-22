21a37d678b69a5c75a53299dcc42f53dbc793ba2
LordHansCapon <walrus.grease2@gmail.com>
2020-12-22 22:56:36 +0100

    Added remove right click context menu option

--- UnofficialCrusaderPatch/Version.cs
+++ UnofficialCrusaderPatch/Version.cs
@@ -3492,6 +3492,19 @@ namespace UCP
                         }
                     }
                 }
+            },
+
+            new Change("o_remove_right_click_context_menu", ChangeType.Other, false)
+            {
+                new DefaultHeader("o_remove_right_click_context_menu")
+                {
+                    // 438B86
+                    new BinaryEdit("o_remove_right_click_context_menu")
+                    {
+                        new BinSkip(29),
+                        0x00
+                    }
+                }
             }
 
             #endregion
