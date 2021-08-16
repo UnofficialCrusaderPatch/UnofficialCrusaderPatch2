33ab16f9df787d6d66cabd71d1f76ccd91710ed1
Liegav <63262183+Liegav@users.noreply.github.com>
2021-08-17 00:17:55 +0200

    Update Localization.cs

--- UnofficialCrusaderPatch/Localization/Localization.cs
+++ UnofficialCrusaderPatch/Localization/Localization.cs
@@ -217,15 +217,7 @@ namespace UCP
                         if (!r.ReadLine(out line) || !line.StartsWith("}"))
                             throw new Exception(ident + " Missing closing bracket at line " + r.LineNumber);
 
-                        //localStrs.Add(ident, text);
-			if (localStrs.ContainsKey(ident))
-                        {
-                            localStrs[ident] = text;
-                        }
-                        else
-                        {
-                            localStrs.Add(ident, text);
-                        }
+                        Add(ident, text);
                     }
                 }
             }
